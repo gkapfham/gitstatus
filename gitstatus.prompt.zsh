e25842cccfa1d614dfffec3a272a23fe2538aa1b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-03-07 15:12:45 -0500

    Move stash status and changes its color in gitstatus.prompt.zsh.

--- gitstatus.prompt.zsh
+++ gitstatus.prompt.zsh
@@ -35,7 +35,8 @@ function gitstatus_prompt_update() {
   [[ $VCS_STATUS_RESULT == 'ok-sync' ]] || return 0  # not a git repo
 
   local      clean='%B%F{110}' # bright blue
-  local      ready='%B%F{96}' # bright blue
+  local      ready='%B%F{96}' # purple
+  local      stash='%B%F{108}' # bright blue
   local   modified='%172F' # orange
   local  untracked='%172F' # orange
   local conflicted='%167F' # dark pink
@@ -56,6 +57,8 @@ function gitstatus_prompt_update() {
   (( $#where > 32 )) && where[13,-13]="…"  # truncate long branch names and tags
   p+="${clean}שׂ ${where//\%/%%}"             # escape %
 
+  # *42 if have stashes.
+  (( VCS_STATUS_STASHES        )) && p+=" ${stash}*${VCS_STATUS_STASHES}"
   # ⇣42 if behind the remote.
   (( VCS_STATUS_COMMITS_BEHIND )) && p+=" ${ready}${VCS_STATUS_COMMITS_BEHIND} %{$fg_bold[green]%}%{✔%G%}"
   # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
@@ -66,8 +69,6 @@ function gitstatus_prompt_update() {
   (( VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND )) && p+=" "
   # ⇢42 if ahead of the push remote; no leading space if also behind: ⇠42⇢42.
   (( VCS_STATUS_PUSH_COMMITS_AHEAD  )) && p+="${ready}${VCS_STATUS_PUSH_COMMITS_AHEAD}"
-  # *42 if have stashes.
-  (( VCS_STATUS_STASHES        )) && p+=" ${clean}*${VCS_STATUS_STASHES}"
   # 'merge' if the repo is in an unusual state.
   [[ -n $VCS_STATUS_ACTION     ]] && p+=" ${conflicted}${VCS_STATUS_ACTION}"
   # ~42 if have merge conflicts.
