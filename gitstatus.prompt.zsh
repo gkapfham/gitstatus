c6ed483d378f701e3b22f7db73f0fa8ae98c01dc
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-03-07 15:55:24 -0500

    Use a user and file icon in prompt of gitstatus.prompt.zsh.

--- gitstatus.prompt.zsh
+++ gitstatus.prompt.zsh
@@ -110,7 +110,7 @@ setopt no_prompt_bang prompt_percent prompt_subst
 #
 # The current directory gets truncated from the left if the whole prompt doesn't fit on the line.
 # PROMPT='%70F%n@%m%f '                                  # green user@host
-PROMPT='%{$fg_bold[yellow]%}gkapfham in %{$fg_bold[green]%}%2~ $(virtualenv_prompt_info)%{$reset_color%}$GITSTATUS_PROMPT%{$reset_color%}'
+PROMPT='%{$fg_bold[yellow]%} gkapfham %{$fg_bold[green]%} %2~ $(virtualenv_prompt_info)%{$reset_color%}$GITSTATUS_PROMPT%{$reset_color%}'
 
 # PROMPT+='%39F%$((-GITSTATUS_PROMPT_LEN-1))<…<%~%<<%f'  # blue current working directory
 # PROMPT+='${GITSTATUS_PROMPT:+ $GITSTATUS_PROMPT}'      # git status
